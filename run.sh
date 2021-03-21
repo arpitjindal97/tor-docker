887b43e7a9f4084068272b5e18590c662bbfefa5
Arpit Agarwal <arpit.agarwal97@protonmail.com>
2021-03-21 22:31:40 +0530

    Separate ID and Remote addr
    Sometimes, strongswan is unable to resolve Remote addr when domain is
    given. Separating them removes the dependency from DNS server
    
    Hence fixing the issue

--- run.sh
+++ run.sh
@@ -2,17 +2,19 @@
 
 ## Environemnt variables explanied
 #
-# VPN		-	(boolean) enable or disable vpn
-# SERVER	-	(string) URL of the server to connect to
-# USER		-	(string) username of VPN server
-# PASS		-	(string) password of VPN server
+# VPN			-	(boolean) enable or disable vpn
+# SERVER_HOST	-	(string) Hostname of the server to connect to
+# SERVER_IP		-	(string) IPv4 addr of the server to connect to
+# USER			-	(string) username of VPN server
+# PASS			-	(string) password of VPN server
 
 if ($VPN);
 then
 	echo "Enabling VPN"
     echo "$USER : EAP \"$PASS\""  > /etc/ipsec.secrets
     sed -i "s/USER/$USER/g" /etc/ipsec.conf
-    sed -i "s/SERVER/$SERVER/g" /etc/ipsec.conf
+    sed -i "s/SERVER_IP/$SERVER_IP/g" /etc/ipsec.conf
+    sed -i "s/SERVER_HOST/$SERVER_HOST/g" /etc/ipsec.conf
     ipsec start
 fi
 sleep 10
